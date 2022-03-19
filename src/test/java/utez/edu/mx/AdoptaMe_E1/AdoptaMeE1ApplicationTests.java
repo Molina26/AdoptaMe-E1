ef04e4d0189a2fba28706493b4718110d571ece3
Manuel A. Molina <maldana261200@gmail.com>
2022-03-19 02:26:54 -0600

    first commit

---
+++ src/test/java/utez/edu/mx/AdoptaMe_E1/AdoptaMeE1ApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.AdoptaMe_E1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaMeE1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
