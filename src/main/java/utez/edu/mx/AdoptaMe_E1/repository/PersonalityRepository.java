c13d4a09e4ffd82bdfe035f070d820791337bc88
Manuel A. Molina <68267658+Molina26@users.noreply.github.com>
2022-03-26 22:08:01 -0600

    adding repositories & services to Color, Personality and Size

---
+++ src/main/java/utez/edu/mx/AdoptaMe_E1/repository/PersonalityRepository.java
@@ -0,0 +1,9 @@
+package utez.edu.mx.AdoptaMe_E1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import utez.edu.mx.AdoptaMe_E1.entity.Personality;
+
+@Repository
+public interface PersonalityRepository extends JpaRepository<Personality, Long> {
+}
