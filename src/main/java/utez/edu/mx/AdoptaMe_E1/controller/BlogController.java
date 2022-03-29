834827929efce94699c426f0888bb2ff0338824d
UrielRivera <20193tn094@utez.edu.mx>
2022-03-29 17:09:59 -0600

    advance save blog, add user

--- src/main/java/utez/edu/mx/AdoptaMe_E1/controller/BlogController.java
+++ src/main/java/utez/edu/mx/AdoptaMe_E1/controller/BlogController.java
@@ -2,18 +2,23 @@ package utez.edu.mx.AdoptaMe_E1.controller;
 
 
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.web.multipart.MultipartFile;
 import utez.edu.mx.AdoptaMe_E1.entity.Blog;
 import utez.edu.mx.AdoptaMe_E1.service.BlogServiceImpl;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping(value = "/blog")
@@ -46,16 +51,23 @@ public class BlogController {
 
     @Secured({"ROLE_ADMINISTRADOR"})
     @GetMapping("/created")
-    public String createdBlog(Blog blog, Model model){
-
+    public String createdBlog(Blog blog, Model model ){
+        model.addAttribute("blog", blog);
         return "views/auth/blog/blogForm";
     }
 
     @Secured({"ROLE_ADMINISTRADOR"})
     @PostMapping("/save")
-    public String saveBlog(Blog blog, Model model){
+    public String saveBlog(Blog blog, BindingResult result, Model model, @RequestParam("image") MultipartFile multipartFileBlog){
+
+
+        blog.setImage(multipartFileBlog.getOriginalFilename());
+
+        blog.setCreatedAt(new Date());
+        blog.setUser();
+
         blogServiceImpl.saveBlog(blog);
-        return "";
+        return "views/auth/blog/blogList";
     }
 
     @Secured({"ROLE_ADMINISTRADOR"})
